449da926033866f9102281936b33db5ffbcf3eed
Pierre Mazurier <pierre.mazurier@viacesi.fr>
2018-02-11 16:28:06 +0100

    Mission B1 - Done

---
+++ Missions B/Mission B1.py
@@ -0,0 +1,21 @@
+import cv2
+from matplotlib import pyplot as plt
+
+img = cv2.imread('img/Gliese 667Cc_surface.pbm', 0)
+
+
+def showPlot(image):
+    plt.hist(img.ravel(), 256, [0, 256])
+    plt.show()
+
+
+#cv2.imshow('img/Encelade_surface.pbm', img)
+#img = cv2.imread('img/Gliese 667Cc_surface.pbm', 0)
+#cv2.normalize(img, img, 255, 255, 1)  # Normalize
+img = cv2.equalizeHist(img)  # Equalize
+cv2.imshow('Modified Image', img)
+
+showPlot(img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+cv2.destroyAllWindows()
